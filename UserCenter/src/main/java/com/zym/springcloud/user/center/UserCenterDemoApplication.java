bfc10434faef81a395100da66a3ffc42c793b73d
zhongym <13138794688@qq.com>
2018-07-06 16:01:30 +0800

    feign - gateway

--- UserCenter/src/main/java/com/zym/springcloud/user/center/UserCenterDemoApplication.java
+++ UserCenter/src/main/java/com/zym/springcloud/user/center/UserCenterDemoApplication.java
@@ -3,12 +3,13 @@ package com.zym.springcloud.user.center;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableCircuitBreaker
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserCenterDemoApplication {
 
