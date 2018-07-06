076c06c2116dcd01297ed81254d081826beaf49d
zhongym <13138794688@qq.com>
2018-07-06 18:33:48 +0800

    feign - gateway用redis限流

--- gateway/src/main/java/com/zym/springcloud/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/zym/springcloud/gateway/GatewayApplication.java
@@ -3,6 +3,9 @@ package com.zym.springcloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 /**
  * @author zhongym
@@ -13,6 +16,12 @@ public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean(name = "apiKeyResolver")
+    public KeyResolver apiKeyResolver() {
+        //根据api接口来限流
+        return exchange -> Mono.just(exchange.getRequest().getPath().value());
     }
 
 }
