12e8f324fbf030292cb5b6414085af959a7ba0b6
Yuanmao.Zhong <yuanmao.zhong@yatsenglobal.com>
2020-04-13 11:47:49 +0800

    nacos

--- ActivityCenter/src/main/java/com/zym/springcloud/activity/center/config/SentinelAutoConfiguration.java
+++ ActivityCenter/src/main/java/com/zym/springcloud/activity/center/config/SentinelAutoConfiguration.java
@@ -1,22 +1,15 @@
 package com.zym.springcloud.activity.center.config;
 
-import com.alibaba.cloud.sentinel.SentinelProperties;
-import com.alibaba.csp.sentinel.adapter.servlet.CommonFilter;
 import com.alibaba.csp.sentinel.adapter.servlet.CommonTotalFilter;
 import com.alibaba.csp.sentinel.adapter.servlet.callback.WebCallbackManager;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.Filter;
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 public class SentinelAutoConfiguration{
